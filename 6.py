0849968109683b6f0b47422e68435f765f5304a2
Lerisa <flashiap08@gmail.com>
2026-02-27 22:37:31 +0300

    turtle is very good

---
+++ 6.py
@@ -0,0 +1,57 @@
+#from turtle import *
+#tracer(0)
+#screensize(5000,5000)
+#r=20
+
+#for i in range(70):
+#    fd(8*r);rt(30)
+#up()
+#for x in range(-50,50):
+#    for y in range(-50,50):
+#        goto(x*r,y*r)
+#        dot(3,'red')
+#update()
+
+#from turtle import *
+#tracer(0)
+#screensize(5000,5000)
+#r=30
+
+#for i in range(2):
+#    rt(120);fd(7*r)
+#rt(300)
+#for i in range(2):
+#    rt(120);fd(7*r)
+
+
+#up()
+#for x in range(-15,15):
+#    for y in range(-15,15):
+#        goto(x*r,y*r)
+#        dot(3,'red')
+#update()
+
+#from turtle import *
+
+#tracer(0)
+#lt(90)
+#screensize(10000,10000)
+#r = 20
+
+#rt(315)
+#for i in range(7):
+#    fd(16*r)
+#    rt(45)
+#    fd(8*r)
+#    rt(135)
+
+#up()
+#for x in range(-50,50):
+#    for y in range(-50,50):
+#        goto(x*r,y*r)
+#        dot(3,'pink')
+#update()
+
+
+
+
