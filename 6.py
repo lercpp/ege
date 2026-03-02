72d69fd365dd57e3926afdd60bac48ee67deb5b5
Lerisa <flashiap08@gmail.com>
2026-03-02 13:13:46 +0300

    1 chast gotova

--- 6.py
+++ 6.py
@@ -52,6 +52,200 @@
 #        dot(3,'pink')
 #update()
 
+#from turtle import *
+
+#tracer(0)
+#lt(90)
+#screensize(10000,10000)
+#r=20
+
+#for i in range(2):
+#    fd(9*r);rt(90);fd(15*r);rt(90)
+#up()
+#fd(12*r)
+#rt(90)
+#down()
+#for i in range(2):
+#    fd(6*r);rt(90);fd(12*r);rt(90)
+#up()
+#for x in range(-50,50):
+#    for y in range(-50,50):
+#        goto(x*r,y*r)
+#        dot(3,'red')
+#update()
+from turtle import *
+
+#tracer(0)
+#screensize(5000,5000)
+#lt(90)
+#r=20
+
+#for i in range(6):
+#    fd(25*r);rt(90)
+#up()
+#fd(20*r)
+#lt(90)
+#bk(5*r)
+#down()
+#for i in range(2):
+#    fd(20*r);lt(90);fd(10*r);lt(90)
+#up()
+#for x in range(-50,50):
+#    for y in range(-50,50):
+#        goto(x*r,y*r)
+ #       dot(3,'red')
+#update()
+
+#tracer(0)
+#screensize(5000,5000)
+#lt(90)
+#r=20
+#for i in range(2):
+#    fd(24*r);rt(90);fd(16*r);rt(90)
+#up()
+#fd(10*r)
+#rt(90)
+#fd(8*r)
+#lt(90)
+#down()
+#for i in range(2):
+#    fd(15*r);rt(90);fd(28*r);rt(90)
+#up()
+#for x in range(-50,50):
+#    for y in range(-50,50):
+#        goto(x*r,y*r)
+#        dot(3,'red')
+#update()
+
+#tracer(0)
+#screensize(5000,5000)
+#lt(90)
+#r=20
+
+#for i in range(4):
+#    fd(10*r);rt(270)
+#up()
+#fd(3*r)
+#rt(270)
+#fd(5*r)
+#rt(90)
+#down()
+#for i in range(2):
+#    fd(10*r);rt(270);fd(12*r);rt(270)
+#up()
+#for x in range(-50,50):
+#    for y in range(-50,50):
+#        goto(x*r,y*r)
+#        dot(3,'red')
+#update()
+
+#tracer(0)
+#screensize(5000,5000)
+#lt(90)
+#r=20
+
+#for i in range(2):
+#    fd(5*r);rt(90);fd(15*r);rt(90)
+#up()
+#fd(-7*r)
+#rt(90)
+#fd(12*r)
+#lt(90)
+#down()
+#for i in range(2):
+#    fd(65*r);rt(90);fd(120*r);rt(90)
+#up()
+#for x in range(-50,50):
+#    for y in range(-50,50):
+#        goto(x*r,y*r)
+#        dot(3,'red')
+
+#update()
+
+#tracer(0)
+#screensize(5000,5000)
+#lt(90)
+#r=20
+#for i in range(3):
+#    down()
+#    for j in range(2):
+#        fd(7*r);rt(90);fd(7*r);rt(90)
+#    up()
+#    fd(6*r)
+#    rt(90)
+#    fd(6*r)
+#    lt(90)
+
+#up()
+#for x in range(-50,50):
+#    for y in range(-50,50):
+#        goto(x*r,y*r)
+#        dot(3,'red')
+#update()
+
+#tracer(0)
+#screensize(50000,50000)
+#lt(90)
+#r=20
+
+#for i in range(4):
+#    fd(19*r);rt(90);fd(30*r);rt(90)
+#up()
+#fd(2*r)
+#rt(90)
+#fd(8*r)
+#lt(90)
+#down()
+#for i in range(4):
+#    fd(93*r);rt(90);fd(97*r);rt(90)
+#up()
+#for x in range(-50,50):
+#    for y in range(-50,50):
+#        goto(x*r,y*r)
+#        dot(3,'red')
+#update()
+
+#tracer(0)
+#screensize(5000,5000)
+#lt(90)
+#r=20
+
+#for i in range(8):
+#    for j in range(4):
+#        fd(5*r);rt(30);fd(6*r);rt(150)
+#    rt(60)
+#up()
+#for x in range(-50,50):
+#    for y in range(-50,50):
+#        goto(x*r,y*r)
+#        dot(3,'red')
+#update()
+
+tracer(0)
+lt(90)
+screensize(10000,10000)
+r = 20
+
+for i in range(2):
+    fd(24*r)
+    rt(90)
+    fd(10*r)
+    rt(90)
+fd(3*r)
+lt(90)
+fd(13*r)
+rt(90)
+for i in range(2):
+    fd(9*r)
+    rt(90)
+    fd(32*r)
+    rt(90)
+up()
+for x in range(-50,50):
+    for y in range(-50,50):
+        goto(x*r,y*r)
+        dot(3,'red')
+update()
 
 
 
