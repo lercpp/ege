9017f971a2fa54d38cd4ee9db3770f3ba26a7433
Lerisa <flashiap08@gmail.com>
2026-03-05 23:34:32 +0300

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,35 @@
+#print(*[f'{x:08b}' for x in (255,255,254,0)])
+#print( )
+#print(*[f'{x:08b}' for x in (208,207,224,0)])
+
+#from ipaddress import *
+#for mask in range(33):
+#     net =ip_network(f'215.181.200.27/{mask}',0)
+#     print(net,net.netmask)
+
+#from ipaddress import *
+#for mask in range(33):
+#    net=ip_network(f'142.198.113.106/{mask}',0)
+#    print(net,mask)
+
+#from ipaddress import *
+#for mask in range(33):
+#    net = ip_network(f'241.185.253.57/{mask}',0)
+#    print(net,32-mask)
+
+#from ipaddress import *
+#for mask in range(33):
+#    net = ip_network(f'108.133.75.91/{mask}',0)
+#    print(net, 2**(32-mask))
+
+#from ipaddress import *
+#for mask in range(33):
+#    net = ip_network(f'157.127.182.76/{mask}',0)
+#    net1 = ip_network(f'157.127.190.80/{mask}',0)
+#    if net!=net1:
+#        print(mask)
+
+#from ipaddress import *
+#net = ip_network(f'102.161.200.51/255.255.255.0',0)
+#print(net[-2])
+    
